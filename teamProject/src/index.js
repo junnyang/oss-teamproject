f676258e21b41203826316e59a253e1ee007becb
junnyang <lhr8440@gmail.com>
2024-12-01 00:54:38 +0900

    modify inde.js

--- teamProject/src/index.js
+++ teamProject/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // React Router 사용
+import App from "./App";
+import "./App.css"; // 전체 CSS 파일 가져오기
+
+ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
-         <App />
+      <App />
     </BrowserRouter>
-    
- 
+  </React.StrictMode>,
+  document.getElementById("root") // index.html의 <div id="root"></div>에 렌더링
 );
-reportWebVitals();
